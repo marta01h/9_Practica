74c0ec295ab3b9267beca0686ba8501e917954e8
marta01h <isip_m.a.sargsyan@mpt.ru>
2023-06-09 10:20:31 +0300

    9 практика

---
+++ Практика 9 Почтовый клиент/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MailClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Подписка на событие клика по кнопке авторизации
+            LoginButton.Click += LoginButton_Click;
+
+            // Подписка на событие изменения выбранной папки
+            FoldersListBox.SelectionChanged += FoldersListBox_SelectionChanged;
+
+            // Подписка на событие изменения выбранного письма
+            MailListView.SelectionChanged += MailListView_SelectionChanged;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика авторизации и проверка на успешную авторизацию
+
+            // Если авторизация успешна, скрываем окно авторизации и показываем основное окно
+            LoginGrid.Visibility = Visibility.Collapsed;
+            MainGrid.Visibility = Visibility.Visible;
+        }
+
+        private void FoldersListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Логика загрузки сообщений из выбранной папки
+        }
+
+        private void MailListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Логика отображения выбранного письма
+        }
+    }
+}
+
